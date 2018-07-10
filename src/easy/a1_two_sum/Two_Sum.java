b34fcdd02e5cf2f68495200b8d33b036f7933237
jianghs <jianghs_own@163.com>
2018-07-10 20:40:12 +0800

    tijiao
    Signed-off-by: jianghs <jianghs_own@163.com>

---
+++ src/easy/a1_two_sum/Two_Sum.java
@@ -0,0 +1,85 @@
+package easy.a1_two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ *  You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *  Given nums = [2, 7, 11, 15], target = 9,
+ *  Because nums[0] + nums[1] = 2 + 7 = 9,
+ *  return [0, 1].
+ */
+
+public class Two_Sum {
+
+    /**
+     * solution1 : 双重循环
+     * 时间复杂度 O(n^2)
+     * 空间复杂度 O(1)
+     * */
+
+    public static int[] solution1(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i ++) {
+            for (int j = i + 1; j < nums.length; j ++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * solution2 : two-pass hash table
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(n)
+     * */
+    public static int[] solution2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i ++) {
+            int otherValue = target - nums[i];
+            // 如果Map中存在另一个value 并且该value 对应的key不是当前i
+            if (map.containsKey(otherValue) && map.get(otherValue) != i) {
+                return new int[] {i, map.get(otherValue)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    /**
+     * solution3 : one-pass hash table
+     * 时间复杂度 O(n)
+     * 空间复杂度 O(n)
+     * */
+    public static int[] solution3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++) {
+            int otherValue = target - nums[i];
+            // 如果Map中存在另一个value
+            if (map.containsKey(otherValue)) {
+                return new int[] {i, map.get(otherValue)};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    public int[] twoSum(int[] nums, int target) {
+        return solution3(nums, target);
+    }
+
+    public static void main(String[] args) {
+        Two_Sum two_sum = new Two_Sum();
+        int[] nums = {55 ,7 ,89 , 9, 6, 22, 45, 1, 11};
+        int target = 51;
+        int[] result = two_sum.twoSum(nums, target);
+        System.out.println(result[0] + " , " + result[1]);
+    }
+}
---
+++ src/easy/a2_reverse_integer/Reverse_Integer.java
@@ -0,0 +1,67 @@
+package easy.a2_reverse_integer;
+
+import java.util.ArrayList;
+
+/**
+ * Reverse digits of an integer.
+ * Example1: x = 123, return 321
+ * Example2: x = -123, return -321
+ * */
+
+public class Reverse_Integer {
+
+
+    public int reverse(int x) {
+        try {
+            int val = x;
+
+            if (x < 0) {
+                val = -x;
+            }
+
+            String str = String.valueOf(val);
+            char[] arr = str.toCharArray();
+
+            String result = "";
+            boolean tZeroflag = false;
+            for (int i = arr.length - 1; i >= 0; i--) {
+                // 如果值为0，且 这个0是最开始的值，则不拼接
+                if (i == arr.length - 1 && !String.valueOf(arr[i]).equals("0")) {
+                    tZeroflag = true;
+                }
+                if (String.valueOf(arr[i]).equals("0") && !tZeroflag) {
+                    tZeroflag = true;
+                    continue;
+                }
+                result += arr[i];
+            }
+            if ("".equals(result)) {
+                return 0;
+            }
+            return x < 0 ? -Integer.parseInt(result) : Integer.parseInt(result);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
+
+    public int reverse1(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) {return 0;}
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) {return 0;}
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        Reverse_Integer t = new Reverse_Integer();
+        int a = -1321;
+        System.out.println(t.reverse1(a));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+    }
+
+}
